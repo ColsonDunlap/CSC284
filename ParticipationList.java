c7954bae57ba8d4ac80dfc100f343ae34487b3c5
dpham013 <72476858+dpham013@users.noreply.github.com>
2022-02-06 21:13:41 -0500

    Update student name

--- ParticipationList.java
+++ ParticipationList.java
@@ -22,6 +22,7 @@ public class ParticipationList {
       students.add(new Student("Rosario", "Patricia"));
       students.add(new Student("Aimy", "Kohli"));
      students.add(new Student("Gilmari", "Velazco"));
+      students.add(new Student("Duy", "Pham"));
 
       //Sort the list by last name
       students.sort((o1,o2) -> o1.getLastName().compareTo(o2.getLastName()));
