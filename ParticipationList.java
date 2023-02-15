ff22574fa505265ea6aefdfa0d6f7e1b524831df
tristr0n <tristr0n.miller@gmail.com>
2023-02-15 13:16:00 -0500

    Update ParticipationList.java
    added triston miller student

--- ParticipationList.java
+++ ParticipationList.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class ParticipationList {
 
-   public static void main(String[] args) {
+   public static void main(String[] args) { 
       
       ArrayList<Student> students = new ArrayList<>();
       
